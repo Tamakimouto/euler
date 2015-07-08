71bc18f53f8badd8687a3f27f8e86bbb291c1dbb
Anthony Zheng <tamakimouto@gmail.com>
2015-07-07 20:45:58 -0400

    number 1

---
+++ 001/main.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+
+  int sum = 0;
+
+  int x = 0;
+  while (3 * x < 1000){
+    sum += 3 * x;
+    x++;
+  }
+  
+  x = 0;
+  while (5 * x < 1000){
+    sum += 5 * x;
+    x++;
+  }
+
+  x = 0;
+  while (15 * x < 1000){
+    sum -= 15 * x;
+    x++;
+  }
+  
+  printf("%d\n", sum);
+
+  return 0;
+}
